ddf20b1f75e1474e14e4c0a5ca385717b13e43d5
Ihor Gayduk <htgroup.dev@gmail.com>
2014-09-18 20:37:09 +0300

    up layout

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -6,6 +6,15 @@
  */
 
 module.exports = {
-	
+
+    'new': function(req, res){
+        res.view();
+    },
+    create: function(req, res, next){
+        User.create(req.params.all(), function userCreated(err, user){
+            if(err) return next(err);
+            res.json(user);
+        });
+    }
 };
 
